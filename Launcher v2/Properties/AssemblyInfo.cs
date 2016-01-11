b34f76b23eca22c72e9d04928ea1044b94a28d29
Alex Sinnott <chess2ryme@gmail.com>
2016-01-11 12:42:33 +1100

    Internal Release 0.1.1

--- Launcher v2/Properties/AssemblyInfo.cs
+++ Launcher v2/Properties/AssemblyInfo.cs
@@ -1,11 +1,12 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AAF Updater")]
+[assembly: AssemblyTitle("AAF_Launcer_Release_0.1.1")]
 [assembly: AssemblyDescription("Updater for the ARMA 3 Modding Platform - Australian Armed Forces")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Australian Armed Forces")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-AU")]
+
