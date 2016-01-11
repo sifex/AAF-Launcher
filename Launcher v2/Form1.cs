ba8612c822724a332126cbbf8ae092eb4546b442
Alex Sinnott <chess2ryme@gmail.com>
2016-01-11 15:20:33 +1100

    Added IE Adding before launch. Here goes nothing.

--- Launcher v2/Form1.cs
+++ Launcher v2/Form1.cs
@@ -143,26 +143,34 @@ namespace Launcher_v2
 
         private void changeIEVersion()
         {
-            int RegVal;
-            int BrowserVer = 11;
 
-            
-            // Set the appropriate IE version
-            if (BrowserVer >= 11)
-                RegVal = 11001;
-            else if (BrowserVer == 10)
-                RegVal = 10001;
-            else if (BrowserVer == 9)
-                RegVal = 9999;
-            else if (BrowserVer == 8)
-                RegVal = 8888;
-            else
-                RegVal = 7000;
 
-            // set the actual key
-            RegistryKey Key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", true);
-            Key.SetValue(System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe", RegVal, RegistryValueKind.DWord);
-            Key.Close();
+            string regCheck = (string)Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", Process.GetCurrentProcess().ProcessName + ".exe", null);
+            if (regCheck == null) 
+            {
+                int RegVal;
+                int BrowserVer = 11;
+
+                // Set the appropriate IE version
+                if (BrowserVer >= 11)
+                    RegVal = 11001;
+                else if (BrowserVer == 10)
+                    RegVal = 10001;
+                else if (BrowserVer == 9)
+                    RegVal = 9999;
+                else if (BrowserVer == 8)
+                    RegVal = 8888;
+                else
+                    RegVal = 7000;
+
+                // set the actual key
+                RegistryKey Key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", true);
+                Key.SetValue(Process.GetCurrentProcess().ProcessName + ".exe", RegVal, RegistryValueKind.DWord);
+                Key.Close();
+                System.Threading.Thread.Sleep(1000);
+                Process.Start(Application.ExecutablePath); // to start new instance of application
+                this.Close(); //to turn off current app
+            }
         }
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
